e9dcba839d98c4c22e51355bd81c8f38d458f375
Andrea <andrea.barnabo92@gmail.com>
2025-04-18 22:30:33 +0200

    gp to bt

--- bt/asgi.py
+++ bt/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for gp project.
+ASGI config for bt project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bt.settings')
 
 application = get_asgi_application()
